76feffeaf69994fe74dcf7efda01c8f773153966
RastDjuss <Folienesie@google.com>
2025-03-03 04:28:26 -0500

    added governance sdk

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: {
-    'postcss-import': {}, // If you're using `import` syntax in CSS.
-    tailwindcss: {},
-    autoprefixer: {}, // Adds vendor prefixes for browser compatibility.
-  },
-};
