4ebb7a60ee7a0e00cc74ccb32736728f3fd0a92c
RastDjuss <Folienesie@google.com>
2025-03-03 08:35:18 -0500

    successfull yarn install, lint and build on to test dev

--- scripts/governance-sdk/spl-governance-data.js
+++
@@ -1,6 +0,0 @@
-// Fetch all Realms accounts (v2)
-const realms = await splGovernance.getAllRealms()
-
-// Fetch proposal from its public key
-const proposalAddress = new PublicKey("4HxrP3R6A6GcUv62VHG331gwJKNhrqHKF438oRztzz2r")
-const proposal = await splGovernance.getProposalByPubkey(proposalAddress)
--- scripts/governance-sdk/spl-governance-rpc-connect.js
+++
@@ -1,8 +0,0 @@
-import { SplGovernance } from "governance-idl-sdk";
-import { Connection } from "@solana/web3.js";
-
-const connection = new Connection("RPC_ENDPOINT");
-const splGovernance = new SplGovernance(
-  connection,
-  optionalProgramId // if custom governance program is used
-);
